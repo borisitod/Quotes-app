3405ab92a8757aedb0a4968824193b0b4b1fc9d1
junchi chen <junchichen@junchis-MacBook-Pro.local>
2017-10-08 12:42:26 +1100

    show favorite quotes

--- src/pages/favorites/favorites.ts
+++ src/pages/favorites/favorites.ts
@@ -1,25 +1,19 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import {Quote} from "../../data/quote.interface";
+import {QuotesService} from "../../services/quotes";
 
-/**
- * Generated class for the FavoritesPage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
 
-@IonicPage()
 @Component({
   selector: 'page-favorites',
   templateUrl: 'favorites.html',
 })
+
 export class FavoritesPage {
+  quotes: Quote[];
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor (private quotesService: QuotesService) {}
+
+  ionViewWillEnter() {
+    this.quotes = this.quotesService.getFavoritesQuotes();
   }
-
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad FavoritesPage');
-  }
-
 }
