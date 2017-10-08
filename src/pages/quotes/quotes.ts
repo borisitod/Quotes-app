ba72b7ee9677e08ca28206699904fa709d8f5053
junchi chen <junchichen@junchis-MacBook-Pro.local>
2017-10-08 11:00:28 +1100

    create alert

--- src/pages/quotes/quotes.ts
+++ src/pages/quotes/quotes.ts
@@ -1,24 +1,50 @@
 import {Component, OnInit} from '@angular/core';
-import {NavParams} from "ionic-angular";
+import {AlertController, NavParams} from "ionic-angular";
 import {Quote} from "../../data/quote.interface";
 
 @Component({
-  selector: 'page-quotes',
-  templateUrl: 'quotes.html',
+    selector: 'page-quotes',
+    templateUrl: 'quotes.html',
 })
-export class QuotesPage implements OnInit{
+export class QuotesPage implements OnInit {
 
-  quoteGroup: {category: string, quotes: Quote[], icon: string};
+    quoteGroup: { category: string, quotes: Quote[], icon: string };
 
-  constructor(private navParams: NavParams){}
+    constructor(private navParams: NavParams, private alertCtrl: AlertController) {
+    }
 
-  // ionViewDidLoad(){
-  //   this.quoteGroup = this.navParams.data;
-  // add elvis operate (?) in template to use this approach
-  // }
+    // ionViewDidLoad(){
+    //   this.quoteGroup = this.navParams.data;
+    // add elvis operate (?) in template to use this approach
+    // }
 
-  ngOnInit(){
-      this.quoteGroup = this.navParams.data;
-  }
+    ngOnInit() {
+        this.quoteGroup = this.navParams.data;
+    }
+
+    onAddToFavorite(selectedQuote: Quote) {
+        const alert = this.alertCtrl.create({
+            title: 'Add Quote',
+            subTitle: 'Are you sure?',
+            message: 'Are you sure you want to add the quote?',
+            buttons: [
+                {
+                   text: 'Yes, go ahead',
+                   handler: () => {
+                       console.log('Ok');
+                   }
+                },
+                {
+                    text: 'No, I changed my mind!',
+                    role: 'cancel',
+                    handler: () => {
+                        console.log('Cancel');
+                    }
+                }
+            ]
+        });
+
+        alert.present();
+    }
 
 }
