b729c6f82896d1961f31087d7052242a499211a0
Boris <boris.chen@kdis.com>
2017-10-09 14:42:21 +1100

    create a service to support toggle

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,6 +12,7 @@ import {QuotePage} from "../pages/quote/quote";
 import {SettingsPage} from "../pages/settings/settings";
 import {TabsPage} from "../pages/tabs/tabs";
 import {QuotesService} from "../services/quotes";
+import {SettingsService} from "../services/settings";
 
 @NgModule({
     declarations: [
@@ -41,6 +42,7 @@ import {QuotesService} from "../services/quotes";
         StatusBar,
         SplashScreen,
         QuotesService,
+        SettingsService,
         {provide: ErrorHandler, useClass: IonicErrorHandler}
     ]
 })
--- src/pages/settings/settings.ts
+++ src/pages/settings/settings.ts
@@ -1,10 +1,20 @@
 import { Component } from '@angular/core';
-import { NavController, NavParams } from 'ionic-angular';
+import {Toggle} from "ionic-angular";
+import {SettingsService} from "../../services/settings";
+
 
 @Component({
   selector: 'page-settings',
   templateUrl: 'settings.html',
 })
 export class SettingsPage {
+    constructor(private settingsService: SettingsService) {}
 
+    onToggle(toggle: Toggle) {
+        this.settingsService.setBackgroud(toggle.checked);
+    }
+
+    checkAltBackground() {
+      return this.settingsService.isAltBackground();
+    }
 }
